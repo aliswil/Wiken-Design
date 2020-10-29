304fe75981bf199b3f52915ad4e1f691d6389d16
Alis Wilson <aliswilson@dhcp-10-24-4-220.wlan.ntnu.no>
2020-10-29 12:01:11 +0100

    insertet the logo

--- wiken-design/src/App.tsx
+++ wiken-design/src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./img/logo_hvit.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="logo" src={logo} alt="Logo" />
+        <h1 className="logo-header">To be continued</h1>
       </header>
     </div>
   );
---
+++ wiken-design/www/precache-manifest.9aea94951674ec9d5f9d27b1cb8fd416.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d73a111c7fcd3458ceb1c09ce3ab067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43675247bcb662bd15f4",
+    "url": "/static/css/main.365fb7a4.chunk.css"
+  },
+  {
+    "revision": "5435e0527d3337264174",
+    "url": "/static/js/2.c51e9e10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c51e9e10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43675247bcb662bd15f4",
+    "url": "/static/js/main.25413c12.chunk.js"
+  },
+  {
+    "revision": "2cd4f738cd0357670939",
+    "url": "/static/js/runtime-main.2bb7ae76.js"
+  },
+  {
+    "revision": "8b7e11eb83b80db964d10f3f6307666d",
+    "url": "/static/media/logo_hvit.8b7e11eb.png"
+  }
+]);
